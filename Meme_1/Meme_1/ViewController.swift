7be1370b979094d9bbd5cc33a9d97d5ad3a6ce9b
surnan <forsuresh@yandex.com>
2018-11-13 07:15:24 -0500

    topTextField, bottomTextField
    both added to opening viewcontroller and properly aligned and formatted.

--- Meme_1/Meme_1/ViewController.swift
+++ Meme_1/Meme_1/ViewController.swift
@@ -8,14 +8,69 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
     
+    
+    //MARK:- UITextField
     @IBOutlet weak var imagePickerView: UIImageView!
-    @IBOutlet weak var topTextField: UITextField!
-    @IBOutlet weak var bottomTextField: UITextField!
+    
+    @IBOutlet weak var topTextField: UITextField! {
+        didSet{
+            //topTextField = setupTextField() // ERROR
+            topTextField = setupTextField(textField: topTextField)
+            topTextField.text = "TOP"
+        }
+    }
+    
+    @IBOutlet weak var bottomTextField: UITextField!{
+        didSet{
+            bottomTextField = setupTextField(textField: bottomTextField)
+            bottomTextField.text = "BOTTOM"
+        }
+    }
+    
+    
+    @objc func myTextFieldTextChanged (textField: UITextField) {
+        textField.text =  textField.text?.uppercased()
+    }
+    
+    func textFieldShouldReturn(_ scoreText: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return true
+    }
+    
+    
+    
+    func setupTextField(textField: UITextField) -> UITextField{
+        let tempTextField = textField
+        tempTextField.clearsOnBeginEditing = true
+        let memeTextAttributes:[NSAttributedString.Key: Any] = [
+            NSAttributedString.Key.strokeColor: UIColor.black,
+            NSAttributedString.Key.foregroundColor: UIColor.white,
+            NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            NSAttributedString.Key.strokeWidth: -4.6
+        ]
+        tempTextField.defaultTextAttributes = memeTextAttributes
+        tempTextField.textAlignment = .center
+        return tempTextField
+    }
+    
+    
+    func setupTextField() -> UITextField {
+        let tempTextField = UITextField()
+        tempTextField.clearsOnBeginEditing = true
+        let memeTextAttributes:[NSAttributedString.Key: Any] = [
+            NSAttributedString.Key.strokeColor: UIColor.black,
+            NSAttributedString.Key.foregroundColor: UIColor.white,
+            NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            NSAttributedString.Key.strokeWidth: -4.6
+        ]
+        tempTextField.defaultTextAttributes = memeTextAttributes
+        tempTextField.textAlignment = .center
+        return tempTextField
+    }
     
     var image: UIImage!     //set via UIImagePickerController()
-
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -24,6 +79,13 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor.yellow
+        
+        [topTextField, bottomTextField].forEach{
+            $0.addTarget(self, action: #selector(myTextFieldTextChanged), for: UIControl.Event.editingChanged)
+        }
+        
+        [topTextField, bottomTextField].forEach{$0?.delegate = self}
     }
 
     
@@ -38,7 +100,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBAction func pickAnImageFromCamera(_ sender: Any) {
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = self
-        
         imagePicker.sourceType = .camera
         present(imagePicker, animated: true, completion: nil)    }
     
